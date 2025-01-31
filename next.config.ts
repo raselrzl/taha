7aefa600ae9135a6317659a101375abd91bef204
Rasel Miah <rasel6041@gmail.com>
2025-01-31 07:22:11 +0100

    fixed filter

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        hostname: "a0.muscache.com",
+        protocol: "https",
+        port: "",
+      },
+    ]
+  }
 };
+
 
 export default nextConfig;
