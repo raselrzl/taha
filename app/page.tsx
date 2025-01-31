3dae3da1f765a5513710778a9cf65548523ab736
Rasel Miah <rasel6041@gmail.com>
2025-01-31 07:32:10 +0100

    added suspense

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,14 @@
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
 import { MapFilterItems } from "./components/MapFilterItems";
+import { Suspense } from "react";
 
 export default function Home() {
   return (
     <div className="container mx-auto px-5 lg:px-10">
-      <MapFilterItems />
+      <Suspense fallback={<div>Loading...</div>}>
+        <MapFilterItems />
+      </Suspense>
     </div>
   );
 }
