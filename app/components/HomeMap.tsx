cb139ecb6fd00966fa137ec3041bd0d20c06ce92
Rasel Miah <rasel6041@gmail.com>
2025-02-04 18:36:46 +0100

    added product page

---
+++ app/components/HomeMap.tsx
@@ -0,0 +1,12 @@
+"use client"
+import { Skeleton } from "@/components/ui/skeleton";
+import dynamic from "next/dynamic";
+
+export function HomeMap({ locationValue }: { locationValue: string }) {
+  const LazyMap = dynamic(() => import("@/app/components/Map"), {
+    ssr: false,
+    loading: () => <Skeleton className="h-[50vh] w-full" />,
+  });
+
+  return <LazyMap locationValue={locationValue} />;
+}
